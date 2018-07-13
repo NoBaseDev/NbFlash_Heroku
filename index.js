fa7e080e7efd00e0ae2a932f21df39d81da8b9af
likj81 <likj81@interpark.com>
2018-07-14 00:17:15 +0900

    add asset links json

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/.well-known/assetlinks.json',function(req,res){
+	res.render('.well-known/assetlinks.json');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
