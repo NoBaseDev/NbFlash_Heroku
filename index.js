169e1757f27f28d349791e67f38d3038bad077e8
likj81 <likj81@interpark.com>
2018-07-16 16:32:13 +0900

    modify index.js

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  //response.send('Hello World!')
+	var fs = require("fs");
+	console.log("\n *START* \n");
+	var content = fs.readFileSync("assetlinks.json");
+	response.send(content);
+	console.log("Output Content : \n"+ content);
+	console.log("\n *EXIT* \n");
 })
 
 app.get('/.well-known/assetlinks.json',function(request, response){
